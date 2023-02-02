97151517361607731863ece44920807a9a7f95d0
LiamStanziani <LiamStanziani1@outlook.com>
2023-02-01 21:28:57 -0500

    3 valid isosceles unit tests passed

--- TriangleCheckTests/TriangleTests.cs
+++ TriangleCheckTests/TriangleTests.cs
@@ -27,5 +27,53 @@ namespace TriangleCheckTests
             Assert.AreEqual(expected, actual);
 
         }
+
+        [Test]
+        public void triangle_input3and3and4_verifyIsosceles()
+        {
+            //Arrange
+            int firstSideInput = 3;
+            int secondSideInput = 3;
+            int thirdSideInput = 4;
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void triangle_input15and7and15_verifyIsosceles()
+        {
+            //Arrange
+            int firstSideInput = 15;
+            int secondSideInput = 7;
+            int thirdSideInput = 15;
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void triangle_input30and30and25_verifyIsosceles()
+        {
+            //Arrange
+            int firstSideInput = 30;
+            int secondSideInput = 30;
+            int thirdSideInput = 25;
+            string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
