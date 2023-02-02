3e00e9e25a808f5560bbfc01ee1c2e6dea24fcf6
LiamStanziani <LiamStanziani1@outlook.com>
2023-02-01 21:45:59 -0500

    5 valid scalene unit tests passed

--- TriangleCheckTests/TriangleTests.cs
+++ TriangleCheckTests/TriangleTests.cs
@@ -64,10 +64,90 @@ namespace TriangleCheckTests
         public void triangle_input30and30and25_verifyIsosceles()
         {
             //Arrange
-            int firstSideInput = 30;
+            int firstSideInput = 25;
             int secondSideInput = 30;
-            int thirdSideInput = 25;
+            int thirdSideInput = 30;
             string expected = "Based on two sides being equal, the type of triangle is an ISOSCELES";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void triangle_input1and10and7_verifyScalene()
+        {
+            //Arrange
+            int firstSideInput = 6;
+            int secondSideInput = 10;
+            int thirdSideInput = 7;
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void triangle_input34and25and15_verifyScalene()
+        {
+            //Arrange
+            int firstSideInput = 34;
+            int secondSideInput = 25;
+            int thirdSideInput = 15;
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void triangle_input11and15and23_verifyScalene()
+        {
+            //Arrange
+            int firstSideInput = 11;
+            int secondSideInput = 15;
+            int thirdSideInput = 23;
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void triangle_input20and18and30_verifyScalene()
+        {
+            //Arrange
+            int firstSideInput = 20;
+            int secondSideInput = 18;
+            int thirdSideInput = 30;
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void triangle_input3and9and11_verifyScalene()
+        {
+            //Arrange
+            int firstSideInput = 3;
+            int secondSideInput = 9;
+            int thirdSideInput = 11;
+            string expected = "Based on all three sides being different, the type of triangle is a SCALENE";
 
             //Act
             string actual = Triangle.AnalyzeTriangle(firstSideInput, secondSideInput, thirdSideInput);
